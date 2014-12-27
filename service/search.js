7903530ea7843350b932308c8f8c5f93c862f0cf
Gustavo Souza <gsouza75@gmail.com>
2014-12-27 14:38:07 -0800

    Add category data; method to get host; cleanup.

--- service/search.js
+++ service/search.js
@@ -19,7 +19,11 @@ var proto = {
         return deferred.reject(err);
       }
 
-      var $ = cheerio.load(res[1]);
+      var body = res[1];
+
+      debug('Got %d response:\n%s', status, body);
+
+      var $ = cheerio.load(body);
       var result = $('.content .row').map(function () {
         return getRowData($(this));
       });
@@ -32,27 +36,32 @@ var proto = {
     }
 
     function getRowData($row) {
+      var baesUrl = 'http://' + host;
       var $anchor = $row.find('a.i');
       var $pl = $row.find('.txt .pl');
+      var $cat = $row.find('.l2 .gc');
 
       return {
         pid: $row.attr('data-pid'),
-        href: $anchor.attr('href'),
-        img: $anchor.find('img').attr('src'),
+        href: baesUrl + $anchor.attr('href'),
         price: $anchor.find('.price').text(),
         text: $pl.find('.hdrlnk').text(),
-        time: Date.parse($pl.find('time').attr('datetime'))
+        time: Date.parse($pl.find('time').attr('datetime')),
+        categoryLink: baesUrl + $cat.attr('href'),
+        categoryId: $cat.attr('data-cat'),
+        category: $cat.text()
       };
     }
 
     this.setOptions(options);
 
-    var url = this.getUrl(query);
+    var host = this.getHost();
+    var searchQryUrl = this.getSearchQueryUrl(host, query);
     var deferred = q.defer();
 
-    debug('Searching url %s', url);
+    debug('Search query url: %s', searchQryUrl);
 
-    request(url)
+    request(searchQryUrl)
     .then(handleResponse)
     .catch(handleError)
     .done();
@@ -60,19 +69,24 @@ var proto = {
     return deferred.promise;
   },
 
-  getUrl: function (query) {
+  getHost: function () {
+    return this.options.city + '.craigslist.org';
+  },
+
+  getSearchQueryUrl: function (host, query) {
     query = query || '';
     
     var obj = {
       protocol: 'http',
-      host: this.options.city + '.craigslist.org',
+      host: host,
       pathname: 'search/sss',
       query: { query: query }
     };
 
     this.options.supportedQueryOptions.forEach(function (option) {
-      /* jshint eqnull: true */
       var opt = this.options[option];
+
+      /* jshint eqnull: true */
       if (opt != null) obj.query[option] = opt;
     }, this);
 
